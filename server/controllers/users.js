5e682bac09054c03a5044f377f4a4c0e864688da
Jeffrey <datle425@gmail.com>
2018-03-12 22:20:24 -0700

    Adding comments

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,7 @@ module.exports = {
     search: function(req, res){
     const axios = require('axios');
     var symb = req.query.sym;
-        function find(str){
+        function find(str){     //Putting symbols into an array to search later
             var array = [];
             var count = 0;
             for(var i = 0; i < str.length; i++){
@@ -29,12 +29,12 @@ module.exports = {
     var bigdata = [];
     for(var k = 0; k < stocks.length; k++){
         var find = stocks[k];
-        // Your API key is: 2CFXI8UYE49A0QSW
+        //API request
         axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + find + '&apikey=2CFXI8UYE49A0QSW&datatype=json')
         .then(response => {
-            var arr = [];
-            var arr2 = response.data['Meta Data'];
-            var arr3 = [];
+            var arr = [];                               //array for prices-data
+            var arr2 = response.data['Meta Data'];      //information about the stock
+            var arr3 = [];                              //array for dates
             var count2 = 1;
             var ts = response.data['Time Series (Daily)'];
             for(var idx in ts){
@@ -42,7 +42,7 @@ module.exports = {
                     break;
                 }
                 else{
-                    function rotate(str){
+                    function rotate(str){       //fixing the order of the dates so it's MM/DD/YYYY
                         var newstr = "";
                         str1 = str.slice(0, 4);
                         str2 = str.slice(5, 10)
@@ -54,13 +54,12 @@ module.exports = {
                 }
                 count2++;
             }
-            bigdata.push({arr, arr2, arr3});
+            bigdata.push({arr, arr2, arr3});     //Adding API res
        })
         .catch(error => {
             console.log(error);
         });   
     }
-    setTimeout(function(){ console.log(bigdata); }, 5000);
-    setTimeout(function(){ res.render("results", {bigdata: bigdata}); }, 5000);
+    setTimeout(function(){ res.render("results", {bigdata: bigdata}); }, 5000); //Pushing all of the data to the front page
     },
 }
